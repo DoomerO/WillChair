f7c86a10a73db44afe888278037ffd738cfa3b2d
Doomer_O <109368541+DoomerO@users.noreply.github.com>
2023-06-18 12:44:27 -0300

    Add files via upload

--- TCC-Server/src/controllers/users/usersController.js
+++ TCC-Server/src/controllers/users/usersController.js
@@ -2,7 +2,7 @@ const knex = require('../../database/database');
 const bcrypt = require('bcrypt');
 
 module.exports= {
-    async searchUsers(req, res) {
+    async searchUsers(req, res) { //recebe todos os cadastros de usuário no sistema
         try {
             const result = await knex('User');
             res.status(201).json(result);
@@ -12,32 +12,27 @@ module.exports= {
         }
     },
 
-    async searchUserEmailPassword(req, res) {
+    async searchUserEmailPassword(req, res) { //Autentica um usuário existente
         try {
             const {email} = req.params;
             const {password} = req.params;
 
             const consult = await knex('User').where("user_email", email);
-            const pass = await knex('User').select('user_password').where("user_email", email);
-           
+            
+            const pass = await knex('User').where("user_email", email).select('user_password');
+            return res.status(201).json({msg: "This user does exists" + pass })
+             
             if (consult != "") {
-                const test = bcrypt.compare(password, pass, function(err, res) {
-                    if (err) {
-                        console.log(err)
-                    }
-                    if(res) {
-                        return true;
+                bcrypt.compare(password, pass.toString()).then((result) => {
+                    if(result) {
+                        return res.status(201).json({msg: "This user does exists" })
                     }
                     else {
-                        return false;
+                        return res.status(401).json({msg: "There is no user with this informations" + pass.toString()});
                     }
+                }).catch((err) => {
+                    return res.status(400).json({error: err.mesage})
                 });
-                if (test) {
-                    return res.status(201).json({msg: "This user does exists" + pass})
-                }
-                else {
-                    return res.status(401).json({msg: "There is no user with this informations"});
-                }
             }
             else {
                 return res.status(401).json({msg: "There is no user with this informations"});
@@ -48,7 +43,7 @@ module.exports= {
         }
     },
 
-    async createUser(req, res) {
+    async createUser(req, res) { //cria um novo usuário no sistema
         try {
             const {user_name} = req.body;
             const {user_email} = req.body;
@@ -60,7 +55,7 @@ module.exports= {
             const consult = await knex('User').where("user_email", user_email);
 
             if (consult != "") {
-                return res.status(400).json({msg: "This user alredy exists"});
+                return res.status(401).json({msg: "This user alredy exists"});
             }
            
             await knex('User').insert({
@@ -76,7 +71,59 @@ module.exports= {
         }
     },
 
-    async deleteUser(req, res) {
+    async updateUser(req, res) { //atualiza parâmetros do usuário
+        try {
+            const {email} = req.params;
+            const {user_name} = req.body;
+            const {user_phone} = req.body;
+            const {user_CEP} = req.body;
+            const {user_houseNum} = req.body;
+            const {user_img} = req.body;
+
+            const consult = await knex('User').where("user_email", email);
+            if (consult != "") {
+                await knex("User").update({
+                    user_name,
+                    user_phone,
+                    user_CEP,
+                    user_houseNum,
+                    user_img
+                }).where('user_email', email);
+            }
+            else {
+                return res.status(401).json({msg: "This User does not exists"});
+            }
+            return res.status(201).json({msg: "User Updated Properly"});
+        }
+        catch(error) {
+            return res.status(400).json({error: error.mesage});
+        }
+    },
+
+    async updateUserPassword(req, res) { //Atualiza a senha do usuário
+        try {
+            const {email} = req.params;
+            const {password} = req.body;
+
+            const consult = await knex('User').where("user_email", email);
+            if (consult != "") {
+                const user_password = bcrypt.hashSync(password, 10);
+
+                await knex("User").update({
+                   user_password
+                }).where('user_email', email);
+            }
+            else {
+                return res.status(401).json({msg: "This User does not exists"});
+            }
+            return res.status(201).json({msg: "Password of " + email + " Updated Properly"});
+        }
+        catch(error) {
+            return res.status(400).json({error: error.mesage});
+        }
+    },
+
+    async deleteUser(req, res) { //deleta um usuário  pelo id
         try {
             const {cod} = req.params;
             await knex('User').del().where("user_id", cod);
