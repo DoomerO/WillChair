73a1e7c2b761bf4efa528937b320b18a96d6f161
Doomer_O <109368541+DoomerO@users.noreply.github.com>
2023-06-19 17:42:44 -0300

    Add files via upload

--- TCC-Server/src/routes.js
+++ TCC-Server/src/routes.js
@@ -1,15 +1,22 @@
 const express = require('express');
 
 const controllersUsers = require('./controllers/users/usersController');
+const controllersOffers = require('./controllers/offers/offersController');
 
 const routes = express.Router();
 
 //User Routes
 routes.get('/users', controllersUsers.searchUsers);
-routes.get('/userAuthentication/:email/:password', controllersUsers.searchUserEmailPassword);
+routes.get('/users/:email/:password', controllersUsers.searchUserEmailPassword);
 routes.post('/users', controllersUsers.createUser);
-routes.put('/user/:email', controllersUsers.updateUser);
-routes.put('/userPassword/:email', controllersUsers.updateUserPassword);
-routes.delete('/user/:cod', controllersUsers.deleteUser);
+routes.put('/users/:email', controllersUsers.updateUser);
+routes.put('/usersPassword/:email', controllersUsers.updateUserPassword);
+routes.delete('/users/:cod', controllersUsers.deleteUser);
+
+//Offer Routes
+routes.get('/offers', controllersOffers.searchOffers);
+routes.post('/offers', controllersOffers.createOffer);
+routes.put('/offers/:id', controllersOffers.updateOffer);
+routes.delete('/offers/:id', controllersOffers.deleteOffer);
 
 module.exports = routes;
