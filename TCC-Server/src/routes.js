efc4af600113b56d4827743b16b7c3685ed48000
Doomer_O <109368541+DoomerO@users.noreply.github.com>
2023-06-27 18:16:32 -0300

    Add files via upload

--- TCC-Server/src/routes.js
+++ TCC-Server/src/routes.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const auth = require('./middleware/auth')
+
+//middlewares
+const auth = require('./middleware/auth');
+
+//Controllers
 const controllersUsers = require('./controllers/users/usersController');
 const controllersOffers = require('./controllers/offers/offersController');
 const controllersMessages = require('./controllers/messages/messagesController');
-const { contentSecurityPolicy } = require('helmet');
+const controllersChats = require('./controllers/chats/chatsController');
 
 const routes = express.Router();
 
 //User Routes
-routes.get('/users', controllersUsers.searchUsers);
+routes.get('/users', auth,controllersUsers.searchUsers);
 routes.get('/users/:email/:password', controllersUsers.searchUserEmailPassword);
 routes.post('/users', controllersUsers.createUser);
 routes.put('/users/:email', controllersUsers.updateUser);
@@ -22,10 +26,17 @@ routes.put('/offers/:id', controllersOffers.updateOffer);
 routes.delete('/offers/:id', controllersOffers.deleteOffer);
 
 //Messages Routes
-routes.get('/messages', controllersMessages.getMessages);
-routes.get('/messages/:chat_id', controllersMessages.getMessagesChat);
+routes.get('/messages', controllersMessages.searchMessages);
+routes.get('/messages/:chat_id', controllersMessages.searchMessagesChat);
 routes.post('/messages', controllersMessages.createMessages);
 routes.delete('/messages/:id', controllersMessages.delMessages);
 routes.delete('/messages/:chat_id', controllersMessages.delMessagesChat);
+
+//Chat Routes 
+routes.get('/chats', controllersChats.searchChats);
+routes.get('/chats/:ofr_id', controllersChats.searchChatOffer);
+routes.post('/chats', controllersChats.createChat);
+routes.delete('/chats/:id', controllersChats.deleteChat);
+routes.delete('chats/:ofr_id', controllersChats.deleteChatOffer);
 
 module.exports = routes;
