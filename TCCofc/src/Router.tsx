cc38d4dbaadbafb0323f5da585c70b10f7da51d5
BurstRange <113287175+BrRange@users.noreply.github.com>
2023-06-27 09:24:08 -0300

    Add files via upload

--- TCCofc/src/Router.tsx
+++ TCCofc/src/Router.tsx
@@ -3,12 +3,14 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Search from "./pages/Search";
+import login from "./pages/Loginwip"
 
 const Router = () => {
     return (
         <BrowserRouter>
         <Routes>
             <Route Component={Home} path="/"/>
+            <Route Component={login} path="/loginw"/>
             <Route Component={Login} path="/login"/>
             <Route Component={Search} path="/search"/>
         </Routes>
