3951bd0e6013415e2d2ba5dee04d2a1e862f5d40
BurstRange <113287175+BrRange@users.noreply.github.com>
2023-06-26 21:58:12 -0300

    Add files via upload

---
+++ TCCofc/src/components/Password.tsx
@@ -0,0 +1,22 @@
+import { Input, useBoolean, InputRightAddon, InputGroup } from '@chakra-ui/react'
+
+interface passProps{
+    placeHolder?: string,
+    setTo: Function
+}
+const Password = ({placeHolder, setTo}:passProps) => {
+    //Estado do botão de mostrar senha e valor da senha
+    const [showPassword, setShowPassword] = useBoolean(false)
+    return(
+        //Elemento de input
+        <InputGroup>
+        <Input placeholder={placeHolder ? placeHolder : 'Senha'} type={showPassword ? 'text' : 'password'}
+        onChange={e => setTo(e.target.value)}/>
+        //Botão de mostrar senha
+        <InputRightAddon bg={showPassword ? '#000' : '#fff'} color={showPassword ? '#fff' : '#222'}
+        onClick={setShowPassword.toggle} onMouseOut={setShowPassword.off}>+</InputRightAddon>
+        </InputGroup>
+    )
+}
+
+export default Password
