6cf25259e4e6999eac87425e5f624ea95ff3f144
Doomer_O <109368541+DoomerO@users.noreply.github.com>
2023-06-27 18:15:08 -0300

    Add files via upload

--- TCC-Server/src/middleware/auth.js
+++ TCC-Server/src/middleware/auth.js
@@ -1,19 +1,25 @@
 const jwt = require("jsonwebtoken");
 
 function verifyToken (req, res, next) {
-
-    const tokenHeader = req.headers['authorazition'];
-    const token = tokenHeader && tokenHeader.split(' ')[1];
-    if (!token) {
-        return res.status(403).json({msg : "User must have a valid token"});
-    }
-    jwt.verify(token, process.env.TOKEN_KEY_ACSSES, (error, user) => {
-        if(error) {
-            return res.status(403).json({msg : "This user does not have acsses to the aplication"});
+    try {
+        const tokenHeader = req.headers['authorization']; 
+        const token = tokenHeader && tokenHeader.split(' ')[1];
+        
+        if (!token) {
+            return res.status(403).json({msg : "User must have a valid token"});
         }
-        req.user = user
-        next();
-    });
+        jwt.verify(token, process.env.TOKEN_KEY_ACSSES, (error, user) => {
+            if(error) {
+                return res.status(403).json({msg : "This user does not have acsses to the aplication"});
+            }
+            req.user = user
+            next();
+        });    
+    }
+    catch(error) {
+        return res.status(400);
+    }
+     
 }
 
 module.exports = verifyToken;
