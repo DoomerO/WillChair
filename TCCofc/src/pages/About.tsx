d700b1a57227316424d652a08860a11b2ddbfa30
Mylliee <102260970+Mylliee@users.noreply.github.com>
2023-07-04 19:04:09 -0300

    About.tsx quase completo

--- TCCofc/src/pages/About.tsx
+++ TCCofc/src/pages/About.tsx
@@ -16,18 +16,79 @@ const About = () => {
                     possibilidade de adquirir um esquipamento de acessibilidade</Heading>
                 </Flex>
             </Flex>
-            
-            <Flex w='100%' bg='#fff' h='60vh' align='center' direction='column' _dark={{bg:'#4f4f4f'}}>
-                <Heading as='h1' color='#1976D2' w="38%" mt="3%" align='center' fontSize="40px" _dark={{color:"#0D87d8"}}>Afinal, 
+
+            <Flex w='100%' bg='#fff' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}}>
+            <Heading as='h1' color='#1976D2' w="38%" mt="3%" align='center' fontSize="40px" _dark={{color:"#0D87d8"}}>Afinal, 
                 do que se trata o WillChair?</Heading>
-                <Flex direction='row' w='60%' align='center' mt='3%' h='20vh'>
-                    <Box><BsPeopleFill size='7vh'/></Box>
+                <HStack mt='2%' gap="50" direction={{base: "column", sm: "row"}} >
+                    
+                    <Flex direction='column' align='center' w='35vh' h='55vh'>
+                        <Box w='fit-content'>
+                            <Container p='20px'>
+                                <BsPeopleFill size='7vh'/>
+                            </Container>
+                        </Box>
+                        <Text align='center'>
+                        Somos uma equipe de progamadores que queriam solucionar um problema social através de 
+                        nosso trabalho. Nos esforçamos para criar um sistema que ajudasse aos outros.
+                        </Text>
+                    </Flex>
                     <Spacer/>
-                    <Box><BsGearFill size='7vh'/></Box>
+                    
+                    <Flex direction='column' align='center' w='35vh' h='55vh'>
+                        <Box w='fit-content'>
+                            <Container p='20px'>
+                                <BsGearFill size='7vh'/>
+                            </Container>
+                        </Box>
+                        <Text align='center'>
+                        Basta se cadastrar no WillChair para usá-lo. Assim você realiza o login, você pode navegar 
+                        por inúmeras ofertas de equipamentos de acessibilidade e até mesmo fazer sua própria oferta.
+                        </Text>
+                    </Flex>
                     <Spacer/>
-                    <Box><HiOutlineDesktopComputer size='7vh'/></Box>
-                </Flex>
-            </Flex>    
+                    
+                    <Flex direction='column' align='center' w='38vh' h='55vh'>
+                        <Box w='fit-content'>
+                            <Container p='20px'>
+                                <HiOutlineDesktopComputer size='7vh'/>
+                            </Container>
+                        </Box>
+                        <Text align='center'>
+                        Nossa aplicação permite que quem precisa de itens de acessibilidade ache ofertantes  
+                        destes produtos. Além disso, o WillChair apresenta três tipos de oferta: doação, venda e 
+                        emprésticmo, se adequando situações financeiras diferentes 
+                        </Text>
+                    </Flex>
+                </HStack>
+            </Flex>
+
+            <Flex w='100%' bg='#F7F9FC' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}}>
+            <Heading as='h1' color='#1976D2' w="38%" mt="3%" align='center' fontSize="40px"
+             _dark={{color:"#0D87d8"}}>Nossa história</Heading>
+                <HStack mt='2%' gap="50" direction={{base: "column", sm: "row"}} >
+                    
+                    <Flex direction='column' w='72vh' h='50vh'>
+                        <Text align='left' fontSize='18px'>
+                        A ideia de desenvolver uma aplicação web que facilitaria a aquisição de
+                        itens de acessibilidade surgiu da necessidade. Nós, da equipe, nos depararmos com uma 
+                        situação em que uma conhecida precisava de uma cadeira de rodas rapidamente e não encontrava 
+                        em lugar nenhum. Ela buscou por dias e não encontrou ninguém que vendesse ou doasse o produto. 
+                        </Text>
+                    </Flex>
+                    <Spacer/>
+                    
+                    <Flex direction='column' w='65vh' h='50vh'>
+                        <Text align='left' fontSize='18px'>
+                        Pensamos juntos em soluções para tal problema e o que surgiu foi o WillChair: uma aplicação 
+                        web que permitiria que o usuário buscando um produto se comunicasse com um ofertante dele. 
+                        Além disso, o WillChair aceitaria três tipos de ofertas: doação, aluguél e venda, para que 
+                        todos consigam suprir sua necessidade de acordo com as circunstâncias em que se encontram. 
+                        </Text>
+                    </Flex>
+                </HStack>
+            </Flex>
+
 
 
 
