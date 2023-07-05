82d07c395267153a0e2d4b9cee19c2187c091c79
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-07-05 17:19:27 -0300

    Correções e aplicação de responsiviade
    Em About.tsx e Home.tsx

--- TCCofc/src/pages/About.tsx
+++ TCCofc/src/pages/About.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Spacer, Text, Image, Heading, HStack, Container } from '@chakra-ui/react';
+import { Box, Flex, Spacer, Text, Heading, Stack, Container } from '@chakra-ui/react';
 import Header from '../components/Header';
 import Footer from "../components/Footer";
 import {BsPeopleFill} from "react-icons/bs";
@@ -9,67 +9,65 @@ const About = () => {
     return (
         <Box w="100%" h="100%">
             <Header/>
+
             <Flex w='100%' h='70vh' bg='#F7F9FC' align='center' _dark={{bg:'#484A4D'}}>
                 <Flex align='center' direction='column' w='100%'>
-                    <Heading color='#1976D2' as='h1' fontSize="40px" _dark={{color:"#0D87d8"}}>WillChair</Heading>
-                    <Heading color='#2D3748' as='h2' fontSize="35px" textAlign='center' noOfLines={4} w="37%">O sonho de fornecer para todos a 
-                    possibilidade de adquirir um esquipamento de acessibilidade</Heading>
+                    <Heading color='#1976D2' as='h1' fontSize="40px">WillChair</Heading>
+                    <Heading color='#2D3748' as='h2' fontSize="35px" textAlign='center' noOfLines={{base: 6, sm: 4}} w={{base: "98%", sm:"37%"}} _dark={{color:"#8f9bbc"}}>O sonho de fornecer para todos a possibilidade de adquirir um esquipamento de acessibilidade!</Heading>
                 </Flex>
             </Flex>
 
-            <Flex w='100%' bg='#fff' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}}>
-            <Heading as='h1' color='#1976D2' w="38%" mt="3%" align='center' fontSize="40px" _dark={{color:"#0D87d8"}}>Afinal, 
-                do que se trata o WillChair?</Heading>
-                <HStack mt='2%' gap="50" direction={{base: "column", sm: "row"}} >
+            <Flex w='100%' bg='#fff' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}} pb='5vh'>
+            <Heading as='h1' color='#1976D2' w={{base:"100%",sm:"38%"}} mt="3%" align='center' fontSize="40px" _dark={{color:"#0D87d8"}}>Afinal, do que se trata o WillChair?</Heading>
+                <Stack mt='3%' gap="50" direction={{base: "column", sm: "row"}} >
                     
-                    <Flex direction='column' align='center' w='35vh' h='55vh'>
+                    <Flex direction='column' align='center' w='35vh' h={{base:'fit-content' ,sm:'55vh'}}>
                         <Box w='fit-content'>
                             <Container p='20px'>
                                 <BsPeopleFill size='7vh'/>
                             </Container>
                         </Box>
-                        <Text align='center'>
+                        <Text align='center' textAlign='justify' fontSize={{base: '19px', sm: '16px'}}>
                         Somos uma equipe de progamadores que queriam solucionar um problema social através de 
                         nosso trabalho. Nos esforçamos para criar um sistema que ajudasse aos outros.
                         </Text>
                     </Flex>
                     <Spacer/>
                     
-                    <Flex direction='column' align='center' w='35vh' h='55vh'>
+                    <Flex direction='column' align='center' w='35vh' h={{base:'fit-content' ,sm:'55vh'}}>
                         <Box w='fit-content'>
                             <Container p='20px'>
                                 <BsGearFill size='7vh'/>
                             </Container>
                         </Box>
-                        <Text align='center'>
+                        <Text align='center'  textAlign='justify' fontSize={{base: '19px', sm: '16px'}}>
                         Basta se cadastrar no WillChair para usá-lo. Assim você realiza o login, você pode navegar 
                         por inúmeras ofertas de equipamentos de acessibilidade e até mesmo fazer sua própria oferta.
                         </Text>
                     </Flex>
                     <Spacer/>
                     
-                    <Flex direction='column' align='center' w='38vh' h='55vh'>
+                    <Flex direction='column' align='center' w='35vh' h={{base:'fit-content' ,sm:'55vh'}}>
                         <Box w='fit-content'>
                             <Container p='20px'>
                                 <HiOutlineDesktopComputer size='7vh'/>
                             </Container>
                         </Box>
-                        <Text align='center'>
+                        <Text align='center' textAlign='justify' fontSize={{base: '19px', sm: '16px'}}>
                         Nossa aplicação permite que quem precisa de itens de acessibilidade ache ofertantes  
                         destes produtos. Além disso, o WillChair apresenta três tipos de oferta: doação, venda e 
                         emprésticmo, se adequando situações financeiras diferentes 
                         </Text>
                     </Flex>
-                </HStack>
+                </Stack>
             </Flex>
 
-            <Flex w='100%' bg='#F7F9FC' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}}>
-            <Heading as='h1' color='#1976D2' w="38%" mt="3%" align='center' fontSize="40px"
-             _dark={{color:"#0D87d8"}}>Nossa história</Heading>
-                <HStack mt='2%' gap="50" direction={{base: "column", sm: "row"}} >
+            <Flex w='100%' bg='#F7F9FC' h='fit-content' align='center' direction='column' _dark={{bg:'#484A4D'}} pb={{base:'5vh',sm:'none'}}>
+            <Heading as='h1' color='#1976D2' w={{base:"100%" ,sm:"38%"}} mt="3%" align='center' fontSize="40px"_dark={{color:"#0D87d8"}}>Nossa história</Heading>
+                <Stack mt='3%' gap="50" direction={{base: "column", sm: "row"}} align='center'>
                     
-                    <Flex direction='column' w='72vh' h='50vh'>
-                        <Text align='left' fontSize='18px'>
+                    <Flex direction='column' w={{base:'80vw', sm:'72vh'}} h={{base:'fit-content' , sm:'50vh'}}>
+                        <Text fontSize={{base:'19px', sm:'18px'}} textAlign='justify'>
                         A ideia de desenvolver uma aplicação web que facilitaria a aquisição de
                         itens de acessibilidade surgiu da necessidade. Nós, da equipe, nos depararmos com uma 
                         situação em que uma conhecida precisava de uma cadeira de rodas rapidamente e não encontrava 
@@ -78,19 +76,16 @@ const About = () => {
                     </Flex>
                     <Spacer/>
                     
-                    <Flex direction='column' w='65vh' h='50vh'>
-                        <Text align='left' fontSize='18px'>
+                    <Flex direction='column' w={{base:'80vw', sm:'65vh'}} h={{base:'fit-content' , sm:'50vh'}}>
+                        <Text fontSize={{base:'19px', sm:'18px'}}  textAlign='justify'>
                         Pensamos juntos em soluções para tal problema e o que surgiu foi o WillChair: uma aplicação 
                         web que permitiria que o usuário buscando um produto se comunicasse com um ofertante dele. 
                         Além disso, o WillChair aceitaria três tipos de ofertas: doação, aluguél e venda, para que 
                         todos consigam suprir sua necessidade de acordo com as circunstâncias em que se encontram. 
                         </Text>
                     </Flex>
-                </HStack>
+                </Stack>
             </Flex>
-
-
-
 
             <Footer/>
         </Box>
--- TCCofc/src/pages/Home.tsx
+++ TCCofc/src/pages/Home.tsx
@@ -24,49 +24,49 @@ const Home = () => {
 
             <Flex w='100%' h='70vh' bg='#F7F9FC' align='center' _dark={{bg:'#484A4D'}}>
                 <Flex align='center' direction='column' ml={{base: "none", sm: '8%'}} w={{base: "inherit", sm :'50%'}}>
-                    <Heading color='#2D3748' as='h1' fontSize={{base: "26px", sm: "30px"}} _dark={{color:"#0D87d8"}}>O seu sonho acessível</Heading>
-                    <Heading color='#1976D2' as='h1' fontSize={{base: "26px", sm: "30px"}} >perto de você!</Heading>
-                    <Text noOfLines={2} fontSize={{base: '18px',sm:'20px'}} mt='20px' align="center">
+                    <Heading color='#2D3748' as='h1' fontSize={{base: "36px", sm: "30px"}} _dark={{color:"#0D87d8"}}>O seu sonho acessível</Heading>
+                    <Heading color='#1976D2' as='h1' fontSize={{base: "36px", sm: "30px"}} >perto de você!</Heading>
+                    <Text noOfLines={2} fontSize={{base: '22px',sm:'20px'}} mt='20px' align="center">
                         Compre, negocie ou anuncie equipamentos de acessibilidade
                     </Text>
                 </Flex>
                 <Image src={topImage} w='50%' h='100%' objectFit='contain' display={{base: "none", sm: "inherit"}}/>
             </Flex>
 
-            <Flex w='100%' bg='#fff' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}}>
-                <Heading as='h1' noOfLines={1} color='#2D3748' mt={{base:'10%', sm:'3%'}} _dark={{color:"#0D87d8"}} fontSize={{base: "22px", sm: "34px"}}>
+            <Flex w='100%' bg='#fff' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}} pb={{base:"5vh", sm:"none"}}>
+                <Heading as='h1' noOfLines={{base: 2, sm: 1}} color='#2D3748' mt={{base:'10%', sm:'3%'}} _dark={{color:"#0D87d8"}} fontSize={{base: "32px", sm: "34px"}} align='center' w={{base:'98vw', sm:'fit-content'}}>
                     O que você pode encontrar por aqui
                 </Heading>
                 <Stack mt='8%' gap="50" direction={{base: "column", sm: "row"}} >
-                    <Flex direction='column' align='center' w='30vh' h='30vh'>
+                    <Flex direction='column' align='center' w='30vh'  h={{base:'33%' , sm:'30vh'}}>
                         <Box w='fit-content'>
                             <Container borderRadius='100%' bg='#E8F1FA' p='20px' _dark={{bg: '#28616A'}}>
                                 <BsCreditCard size='8vh'/>
                             </Container>
                         </Box>
-                        <Text align='center' noOfLines={2}>
+                        <Text align='center' noOfLines={2} fontSize={{base:'19px', sm:'15px'}}>
                             Compras ou vendas de produtos
                         </Text>
                     </Flex>
                     <Spacer />
-                    <Flex direction='column' align='center' w='30vh' h='30vh'>
+                    <Flex direction='column' align='center' w='30vh' h={{base:'33%' , sm:'30vh'}}>
                         <Box w='fit-content'>
                             <Container borderRadius='100%' bg='#E8F1FA' p='20px' _dark={{bg: '#28616A'}}>
                                 <BiDonateHeart size='8vh'/>
                             </Container>
                         </Box>
-                        <Text align='center' noOfLines={2}>
+                        <Text align='center' noOfLines={2} fontSize={{base:'19px', sm:'15px'}}>
                             Doações de equipamentos
                         </Text>
                     </Flex>
                     <Spacer />
-                    <Flex direction='column' align='center' w='30vh' h='30vh'>
+                    <Flex direction='column' align='center' w='30vh'  h={{base:'33%' , sm:'30vh'}}>
                         <Box w='fit-content'>
                             <Container borderRadius='100%' bg='#E8F1FA' p='20px' _dark={{bg: '#28616A'}}>
                                 <MdWheelchairPickup size='8vh'/>
                             </Container>
                         </Box>
-                        <Text align='center' noOfLines={2}>
+                        <Text align='center' noOfLines={2} fontSize={{base:'19px', sm:'15px'}}>
                             Aluguéis por preços acessíveis
                         </Text>
                     </Flex>
@@ -75,7 +75,7 @@ const Home = () => {
 
             <Flex w='100%' h='fit-content' bg='#F7F9FC' align='center' direction='column' _dark={{bg:'#484A4D'}}>
                 <Box h='20%' alignContent='center' mt={{base:'10%', sm:'3%'}}>
-                <Heading as='h2' noOfLines={2} color='#1963D2' _dark={{color:'#1983D2'}} fontSize={{base: "22px", sm: "34px"}} align="center">
+                <Heading as='h2' noOfLines={{base: 2, sm: 1}} color='#1963D2' _dark={{color:'#1983D2'}} fontSize={{base: "32px", sm: "34px"}} align='center' w={{base:'98vw', sm:'fit-content'}}>
                     Confira abaixo algumas de nossas categorias
                 </Heading>
                 </Box>
