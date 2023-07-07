366b371f03eef2751c0d229967ad420aa07934be
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-07-07 10:43:09 -0300

    Correções Password

--- TCCofc/src/components/Password.tsx
+++ TCCofc/src/components/Password.tsx
@@ -8,11 +8,10 @@ const Password = ({placeholder, setTo}:passProps) => {
     //Estado do botão de mostrar senha e valor da senha
     const [showPassword, setShowPassword] = useBoolean(false)
     return(
-        //Elemento de input
         <InputGroup>
         <Input placeholder={placeholder} type={showPassword ? 'text' : 'password'}
         onChange={e => setTo(e.target.value)}/>
-        //Botão de mostrar senha
+        
         <InputRightAddon bg={showPassword ? '#000' : '#fff'} color={showPassword ? '#fff' : '#222'}
         onClick={setShowPassword.toggle} onMouseOut={setShowPassword.off} cursor='pointer'>+</InputRightAddon>
         </InputGroup>
