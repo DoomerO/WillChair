3019f68fd8a7c36cbed0250139c5d4bbf21928e8
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-07-07 18:51:10 -0300

    Router atualizado

--- TCCofc/src/Router.tsx
+++ TCCofc/src/Router.tsx
@@ -6,20 +6,25 @@ import Search from "./pages/Search";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import login from "./pages/Loginwip"
-import createOffer from "./pages/createOffer";
-import HomeToggle from "./components/HomeToggle";
+import CreateOffer from "./pages/CreateOffer";
+import PageToggle from "./components/PageToggle";
+import HomeProd from "./pages/HomeProd";
 
 const Router = () => {
+    const toggleHome = () => {
+        return <PageToggle compError={<Home />} compSucsses={<HomeProd/>}/>
+    }
+
     return (
         <BrowserRouter>
         <Routes>
-            <Route Component={HomeToggle} path="/"/>
+            <Route Component={toggleHome} path="/"/>
             <Route Component={login} path="/loginw"/>
             <Route Component={Login} path="/login"/>
             <Route Component={Search} path="/search"/>
             <Route Component={About} path="/about"/>
             <Route Component={Contact} path="/contact"/>
-            <Route Component={createOffer} path="/createoffer"/>
+            <Route Component={CreateOffer} path="/createoffer"/>
         </Routes>
     </BrowserRouter>
     )
