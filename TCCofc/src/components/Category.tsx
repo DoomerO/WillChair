03878ada1a7ead9fc630d547e4fa9659443441be
Mylliee <102260970+Mylliee@users.noreply.github.com>
2023-07-07 10:04:22 -0300

    Redirecionamento do caminho

--- TCCofc/src/components/Category.tsx
+++ TCCofc/src/components/Category.tsx
@@ -6,15 +6,16 @@ import "../styles/components/Category.css";
 interface catProps {
     icon: string //Using to define the path to an image to represent the category
     name: string //Using to define the name to a Category
+    path: string //Caminho de redirecionamento da categoria
 }  
 
-const Category =({name, icon}:catProps) => {
+const Category =({name, icon, path}:catProps) => {
 
     return (
        <Flex align='center' direction='column' w={{base:"50vw", sm: "16vw"}} m={{base: "20px", sm: "none"}}>
             <Box w='fit-content' mb='10px'>
                 <Container bg='#fff' p={{base:'10px' ,sm:'20px'}} borderRadius='15px' boxShadow='lg' _hover={{bg : "#ddd"}} _dark={{bg: '#444', _hover : {bg: "#123"}}}>
-                    <Link to='/login'><Image src={icon} objectFit='contain' w="12vh" h="14vh"></Image></Link>
+                    <Link to={path}><Image src={icon} objectFit='contain' w="12vh" h="14vh"></Image></Link>
                 </Container>
             </Box>
             
