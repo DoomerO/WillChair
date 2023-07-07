7cc443fcd3e26d9fb42bf0f06c72bb6c086460c1
BurstRange <113287175+BrRange@users.noreply.github.com>
2023-07-07 08:56:41 -0300

    update login

--- TCCofc/src/pages/Loginwip.tsx
+++ TCCofc/src/pages/Loginwip.tsx
@@ -2,7 +2,7 @@ import '../styles/pages/Login.css';
 import { useState, useEffect } from 'react';
 import Password from '../components/Password';
 import axios from 'axios';
-import { Wrap, Stack, Button, Toast, Collapse, Input, Center, useBoolean } from '@chakra-ui/react'
+import { Wrap, Stack, Button, Toast, Collapse, Input, Center, useBoolean, Container } from '@chakra-ui/react'
 
 const Loginwip = () => {
 
@@ -32,33 +32,41 @@ const Loginwip = () => {
 
     const [screen, setScreen] = useBoolean(false)
 
+    function switchScreen(){
+        setEmail('')
+        setName('')
+        setPassword('')
+        setScreen.toggle
+    }
+
     return (
-        <Center>
+        <Container border='0.5vh solid' borderRadius='10px' marginTop='10vh' maxW='container.md'
+            padding='2vh' height='fit-content' centerContent>
             <Stack id='mainDiv' direction={'row'}>
-                <Collapse in={!screen} >
-                    <Wrap>
-                        <Input className='submit' placeholder='E-mail ou nome de usuário'/>
-                        <Password setTo={setPassword}/>
+                <Collapse in={!screen}>
+                    <Wrap justify='center'>
+                        <Input className='submit' placeholder='E-mail ou nome de usuário' onChange={e => setName(e.target.value)}/>
+                        <Password setTo={setPassword} placeholder='Senha'/>
                     </Wrap>
                     <Wrap justify='center' spacing={8}>
-                        <Button onClick={setScreen.toggle}>Sign Up</Button>
-                        <Button>Enviar</Button>
+                        <Button onClick={switchScreen()}>Sign Up</Button>
+                        <Button type='submit' onClick={() => {handleSubmits()}}>Enviar</Button>
                     </Wrap>
                 </Collapse>
                 <Collapse in={screen}>
-                    <Wrap>
+                    <Wrap justify='center'>
                         <Input className='submit' placeholder='Nome de usuário' onChange={e => setName(e.target.value)}/>
                         <Input className='submit' placeholder='E-mail' onChange={e => setEmail(e.target.value)}/>
-                        <Password setTo={setPassword} placeHolder='Senha'/>
-                        <Password setTo={setPassword} placeHolder='Confirmar senha'/>
+                        <Password setTo={setPassword} placeholder='Senha'/>
+                        <Password setTo={setPassword} placeholder='Confirmar senha'/>
                     </Wrap>
                     <Wrap justify='center' spacing={8}>
-                        <Button onClick={setScreen.toggle}>Login</Button>
+                        <Button onClick={switchScreen()}>Login</Button>
                         <Button type='submit' onClick={() => {handleSubmits()}}>Enviar</Button>
                     </Wrap>
                 </Collapse>
             </Stack>
-        </Center>
+        </Container>
     );
 }
 
