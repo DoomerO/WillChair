e5214cb64751c7d06add97a29f6256ee8d725cf0
Mylliee <102260970+Mylliee@users.noreply.github.com>
2023-07-10 08:50:28 -0300

    Atualização

--- TCCofc/src/pages/wcOffer.tsx
+++ TCCofc/src/pages/wcOffer.tsx
@@ -7,20 +7,21 @@ const wcOffer = () => {
             <Box w="100%" h="100%">
                 <Header/>
     
-                <Flex w='100%' h='40vh' bg='#F7F9FC' align='center' _dark={{bg:'#484A4D'}}>
-                    <Flex align='center' direction='column' w='100%'>
+                <Flex w='100%' h='30vh' bg='#F7F9FC' align='center' _dark={{bg:'#4f4f4f'}}>
+                    <Flex mt='2%' align='center' direction='column' w='100%'>
                         <Heading color='#1976D2' as='h1' fontSize="40px">Descreva seu produto</Heading>
                     </Flex>
                 </Flex>
 
-                <Flex w='100%' bg='#fff' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}} pb={{base:"5vh", sm:"none"}}>
-                <Stack mt='8%' gap="50" direction={{base: "column", sm: "row"}} >
+                <Flex w='100%' bg='#F7F9FC' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}} pb={{base:"5vh", sm:"none"}}>
+                <Stack gap="90" direction={{base: "column", sm: "row"}} >
                     <Flex direction='column' align='center' w='30vh'  h={{base:'33%' , sm:'30vh'}}>
                         <Stack spacing={3}>
-                        <Input placeholder='extra small size' size='xs' />
-                        <Input placeholder='small size' size='sm' />
-                        <Input placeholder='medium size' size='md' />
-                        <Input placeholder='large size' size='lg' />
+                        <Input placeholder='Nome do produto' w='75vh'/>
+                        <Input placeholder='Descrição' size='lg'/>
+                        <Input placeholder='small size' size='sm'/>
+                        <Input placeholder='medium size' size='md'/>
+                        <Input placeholder='large size' size='lg'/>
                         </Stack>
                     </Flex>
                 
@@ -35,3 +36,4 @@ const wcOffer = () => {
 }
 
 export default wcOffer;
+
