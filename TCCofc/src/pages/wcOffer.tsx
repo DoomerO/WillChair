75e8af48686684e3dcd21ac97006f3ca598bc9f4
MatheusBarbosa-01 <101056776+MatheusBarbosa-01@users.noreply.github.com>
2023-07-10 10:21:57 -0300

    Incremento de elementos na página wcOffer.tsx

--- TCCofc/src/pages/wcOffer.tsx
+++ TCCofc/src/pages/wcOffer.tsx
@@ -1,32 +1,53 @@
 import Header from '../components/Header';
 import Footer from "../components/Footer";
 import { Box, Flex, Spacer, Text, Heading, Stack, Container, Input } from '@chakra-ui/react';
+import { Select } from '@chakra-ui/react';
+import { InputGroup, InputLeftElement, InputRightElement} from '@chakra-ui/react';
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 const wcOffer = () => {
     return (
             <Box w="100%" h="100%">
-                <Header/>
-    
-                <Flex w='100%' h='30vh' bg='#F7F9FC' align='center' _dark={{bg:'#4f4f4f'}}>
-                    <Flex mt='2%' align='center' direction='column' w='100%'>
-                        <Heading color='#1976D2' as='h1' fontSize="40px">Descreva seu produto</Heading>
+                    <Header/>
+        
+                    <Flex w='100%' h='30vh' bg='#F7F9FC' align='center' _dark={{bg:'#4f4f4f'}}>
+                        <Flex mt='2%' align='center' direction='column' w='100%'>
+                            <Heading color='#1976D2' as='h1' fontSize="35px">Descreva seu produto</Heading>
+                        </Flex>
                     </Flex>
-                </Flex>
 
-                <Flex w='100%' bg='#F7F9FC' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}} pb={{base:"5vh", sm:"none"}}>
-                <Stack gap="90" direction={{base: "column", sm: "row"}} >
-                    <Flex direction='column' align='center' w='30vh'  h={{base:'33%' , sm:'30vh'}}>
-                        <Stack spacing={3}>
-                        <Input placeholder='Nome do produto' w='75vh'/>
-                        <Input placeholder='Descrição' size='lg'/>
-                        <Input placeholder='small size' size='sm'/>
-                        <Input placeholder='medium size' size='md'/>
-                        <Input placeholder='large size' size='lg'/>
-                        </Stack>
-                    </Flex>
-                
-                </Stack>
-            </Flex>
+                    <Flex w='100%' bg='#F7F9FC' h='fit-content' align='center' direction='column' _dark={{bg:'#4f4f4f'}} pb={{base:"5vh", sm:"none"}}>
+                    <Stack gap="90" direction={{base: "column", sm: "row"}} >
+                        <Flex direction='column' align='center' w='30vh'  h={{base:'33%' , sm:'60vh'}}>
+                            <Stack spacing={3}>
+                                <Input placeholder='Nome do produto' w='75vh'/>
+                                <Input placeholder='Descrição' size='lg' h="20vh" textAlign="left" verticalAlign="top"/>                        
+                                <Select placeholder='Peso' w="20vh" color="gray">
+                                    <option value='option1'>Option 1</option>
+                                    <option value='option2'>Option 2</option>
+                                    <option value='option3'>Option 3</option>
+                                </Select>
+
+                                <Stack spacing={4}>
+                                    <InputGroup>
+                                        <InputLeftElement
+                                            pointerEvents='none'
+                                            color='gray.440'
+                                            fontSize='1.2em'
+                                            children='$'
+                                        />
+                                        <Input placeholder='Preço' />
+                                    </InputGroup>
+                                </Stack>
+
+                            </Stack>
+                            <ButtonGroup variant="outline" spacing='6' margin="5vh">
+                                <Button colorScheme='blue'>Salvar</Button>
+                                <Button>Cancelar</Button>
+                            </ButtonGroup>
+                        </Flex>
+                    </Stack>
+                </Flex>
 
 
                 <Footer/>
