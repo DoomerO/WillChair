113bf53f3709e9abbc814963c0115670c8bf648d
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-07-10 10:01:01 -0300

    Add files via upload

--- TCCofc/src/pages/HomeProd.tsx
+++ TCCofc/src/pages/HomeProd.tsx
@@ -17,20 +17,11 @@ const HomeProd = () => {
 
     const [closeOffers, setClose] = useState([]);
     const [user, setUser] = useState(decode(localStorage.getItem("token")));
-    const [city, setCity] = useState<String>();
+    const [userQuery, setQuery] = useState([]);
 
     async function queryCloseOffers() {
-
-        await axios.get(`http://localhost:3344/users/email/${user.email}`, {headers: {
-                authorization : "Bearer " + localStorage.getItem("token")
-        }}).then(res => {
-            setCity(res.data[0].user_city);
-        }).catch(error => {
-            console.log(error);
-        })
-      
-        await axios.get(`http://localhost:3344/offers/query/${"user_city"}/${city}`, {headers: {
-                authorization : "Bearer " + localStorage.getItem("token")
+        await axios.get(`http://localhost:3344/offers/query/${"user_city"}/${userQuery.user_city}`, {headers: {
+            authorization : "Bearer " + localStorage.getItem("token")
         }}).then(res => {
             setClose(res.data);
             console.log(res)
@@ -39,9 +30,23 @@ const HomeProd = () => {
         })
     }
 
+    async function queryUser() {
+        await axios.get(`http://localhost:3344/users/email/${user.email}`, {headers: {
+            authorization : "Bearer " + localStorage.getItem("token")
+        }}).then(res => {
+            setQuery(res.data[0]);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    useEffect(() => {
+        queryUser();
+    }, []);
+
     useEffect(() => {
         queryCloseOffers();
-    }, []);
+    }, [userQuery]); 
 
     const renderOffers = closeOffers.map(item => {
         return <CardOffer 
@@ -71,7 +76,7 @@ const HomeProd = () => {
 
             <Flex bg="#fff" h='fit-content' align="center" direction="column" _dark={{bg:'#4f4f4f'}} pb='5vh'>
                 <Heading color='#2D3748' as='h1' fontSize={{base: "36px", sm: "30px"}} _dark={{color:"#0D87d8"}} mt="3%" mb="5%"
-                onClick={() => {console.log(city)}}>Confira as ofertas perto de você</Heading>
+                onClick={() => {console.log(userQuery)}}>Confira as ofertas perto de você</Heading>
                     <Tabs alignContent="center">
                         <TabPanels>
                             <TabPanel overflowX="scroll" maxWidth="98vw" css={{
