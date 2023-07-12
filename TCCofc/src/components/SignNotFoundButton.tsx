e2442d6bdda169a55d4e4d493d5c86eeac4f94c8
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-07-11 21:11:57 -0300

    Placas avisos para Homeprod

--- TCCofc/src/components/SignNotFoundButton.tsx
+++ TCCofc/src/components/SignNotFoundButton.tsx
@@ -1,20 +1,24 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import "../fonts/fonts.css";
 
 interface signProps {
     msg : string,
+    btnPath : string,
+    btnText : string,
     icon : React.ReactElement
 }
 
-const SignNotFound = ({msg, icon} : signProps) => {
+const SignNotFoundButton = ({msg, icon, btnPath, btnText} : signProps) => {
     return (
         <Flex align="center" h="40vh" direction="column" 
         bgGradient="linear(to-b, #1976D230, #1976D2)" 
         w="100%" pt="3%">
            {icon}
             <Text fontSize="25px" fontFamily="atkinson">{msg}</Text>
+            <Button variant="outline" bgColor="#0000" mt="1.5%"><Link to={btnPath}>{btnText}</Link></Button>
         </Flex>
     );
 }
 
-export default SignNotFound;
+export default SignNotFoundButton;
