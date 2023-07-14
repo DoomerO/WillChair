0e6b1d004689a759a208518fc88c31eb155a2bd1
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-07-14 19:46:44 -0300

    arquivo contento as cores do projeto
    Objetivo de facilitar modificações futuras, necessária aplicação em cada página

---
+++ TCCofc/src/colors/colors.ts
@@ -0,0 +1,18 @@
+const colors = {
+    veryLightBlue : "#F7F9FC",
+    lightGray_Dark : "#484A4D",
+    bgWhite: "#fff",
+    bgWhite_Dark: "#4f4f4f",
+    black: '#000',
+    colorFontDarkBlue: "#2D3748",
+    colorFontDarkBlue_Dark: "#8f9bbc",
+    colorFontBlue: "#1976D2",
+    containerBg: "#E8F1FA", 
+    containerBg_Dark: "#28616A",
+    signColor: "#1976D2",
+    signColorOpacity: "#1976D230",
+    categoryBg: "#fff",
+    categoryBg_Dark: "#444",
+}
+
+export default colors
