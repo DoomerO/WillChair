c2657aa5178da0efd550e5de59772b1ca02fa0b9
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-07-15 10:42:55 -0300

    Correções e padronização em HomeProd

--- TCCofc/src/pages/HomeProd.tsx
+++ TCCofc/src/pages/HomeProd.tsx
@@ -1,4 +1,4 @@
-import {Box, Heading, Flex, Input, InputGroup, InputRightAddon, Stack, TabPanel, Tabs, TabPanels} from '@chakra-ui/react';
+import {Box, Heading, Flex, Input, InputGroup, InputRightAddon} from '@chakra-ui/react';
 import {useState, useEffect} from 'react';
 
 import decode from '../components/decoderToken';
@@ -16,6 +16,7 @@ import {BsPencil} from "react-icons/bs"
 import SignNotFoundButton from '../components/SignNotFoundButton';
 
 import "../fonts/fonts.css";
+import colors from "../colors/colors";
 
 const HomeProd = () => {
 
@@ -91,9 +92,9 @@ const HomeProd = () => {
         <Box w="100%" h="100%">
             <HeaderLoged name={user.name} img={""}/>
 
-            <Flex bg="#F7F9FC" w='100%' h='70vh' align="center" _dark={{bg:'#484A4D'}}>
+            <Flex bg={colors.veryLightBlue} w='100%' h='70vh' align="center" _dark={{bg:colors.veryLightBlue_Dark}}>
                 <Flex w="100%" direction="column" align="center">
-                    <Heading color='#2D3748' as='h1' fontSize={{base: "36px", sm: "30px"}} fontFamily="outfit" _dark={{color:"#0D87d8"}} mb="5%">O que deseja encontrar?</Heading>
+                    <Heading color={colors.colorFontDarkBlue} as='h1' fontSize={{base: "36px", sm: "30px"}} fontFamily="outfit" _dark={{color:colors.colorFontDarkBlue_Dark}} mb="5%">O que deseja encontrar?</Heading>
                     <InputGroup display="flex" zIndex={1} w="50%">    
                         <Input placeholder='Busque as melhores ofertas aqui!' bg="#eee" borderRightColor="#000" _dark={{bg:"#0000", borderRightColor:"#fff", color: "#fff", _placeholder : {color: "#dfdfdf"}}}/>
                         <InputRightAddon children={<BiSearchAlt/>} bg="#eee" _dark={{bg:"#0000"}}/>
@@ -101,14 +102,14 @@ const HomeProd = () => {
                 </Flex>
             </Flex>
 
-            <Flex bg="#fff" h='fit-content' align="center" direction="column" _dark={{bg:'#4f4f4f'}} pb='5vh'>
-                <Heading color='#2D3748' as='h1' fontSize={{base: "36px", sm: "30px"}} _dark={{color:"#0D87d8"}} mt="3%" mb="5%"
+            <Flex bg={colors.bgWhite} h='fit-content' align="center" direction="column" _dark={{bg:colors.bgWhite_Dark}}>
+                <Heading color={colors.colorFontDarkBlue} as='h1' fontSize={{base: "36px", sm: "30px"}} _dark={{color:colors.colorFontDarkBlue_Dark}} mt="3%" mb="5%"
                 onClick={() => {console.log(userQuery)}} fontFamily="outfit">Confira as ofertas perto de você</Heading>
                 {(closeOffers.length > 0) ? <OfferList component={renderCloseOffers}/> : <SignNotFound msg="Parece que não há equipamentos registrados em sua cidade..." icon={<MdOutlineSearchOff size="45%"/>}/>}
             </Flex>
 
-            <Flex bg="#F7F9FC" w='100%' h='fit-content' align="center" _dark={{bg:'#484A4D'}} pb="5vh" direction="column">
-                <Heading color='#2D3748' as='h1' fontFamily="outfit" fontSize={{base: "36px", sm: "30px"}} _dark={{color:"#0D87d8"}} mt="3%" mb="5%">
+            <Flex bg={colors.veryLightBlue} w='100%' h='fit-content' align="center" _dark={{bg:colors.veryLightBlue_Dark}} direction="column">
+                <Heading color={colors.colorFontDarkBlue} as='h1' fontFamily="outfit" fontSize={{base: "36px", sm: "30px"}} _dark={{color: colors.colorFontDarkBlue_Dark}} mt="3%" mb="5%">
                     Ofertas criadas por você
                 </Heading>
                 {(userOffers.length > 0) ? <OfferList component={renderUserOffers}/> : <SignNotFoundButton msg="Parece que você não possui ofertas registradas...Que tal criar alguma?!" icon={<BsPencil size="45%"/>} btnText='Criar Oferta' btnPath='/createoffer'/>} 
