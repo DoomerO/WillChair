d3850f4f6a82a472b101da0d8c642c1b40466729
BurstRange <113287175+BrRange@users.noreply.github.com>
2023-07-17 16:34:43 -0300

    Campo auto apagável

--- TCCofc/src/components/Password.tsx
+++ TCCofc/src/components/Password.tsx
@@ -2,15 +2,17 @@ import { Input, useBoolean, InputRightAddon, InputGroup } from '@chakra-ui/react
 
 interface passProps{
     placeholder?: string,
-    setTo: Function
+    setTo: Function,
+    value?: string
 }
-const Password = ({placeholder, setTo}:passProps) => {
+const Password = ({placeholder, setTo, value}:passProps) => {
     //Estado do botão de mostrar senha e valor da senha
     const [showPassword, setShowPassword] = useBoolean(false)
+
     return(
         <InputGroup>
         <Input placeholder={placeholder} type={showPassword ? 'text' : 'password'}
-        onChange={e => setTo(e.target.value)}/>
+        onChange={e => setTo(e.target.value)} value={value}/>
         
         <InputRightAddon bg={showPassword ? '#000' : '#fff'} color={showPassword ? '#fff' : '#222'}
         onClick={setShowPassword.toggle} onMouseOut={setShowPassword.off} cursor='pointer'>+</InputRightAddon>
