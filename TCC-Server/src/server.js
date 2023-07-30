1a5b2f0b41a86719d3adf9b9b7ff24eef8579c4b
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-07-30 18:19:15 -0300

    Adição de Socket backend

--- TCC-Server/src/server.js
+++ TCC-Server/src/server.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const helmet = require('helmet');
+const knex = require("./database/database")
 const cors = require('cors');
 const routes = require('./routes');
 
@@ -10,6 +11,33 @@ server.use(cors());
 server.use(helmet());
 server.use(express.json());
 server.use(routes);
+
+const {Server} = require('socket.io');
+
+const io = new Server(3000, {
+    cors : {
+        origin: "http://localhost:5173"
+    }
+});
+
+io.on("connection", (socket) => {
+    socket.emit("message", "Funfa");
+    
+    socket.on("reqMsg", async (data) => {
+        console.log(2)
+        const msgs = await knex("Message").where("Chat_chat_id", data)
+        socket.emit("showMsg", msgs);
+        socket.emit("ganbis", msgs);
+    })
+
+    socket.on("postMessage", async (data) => {
+        await knex("Message").insert({
+            msg_content : data.msg,
+            Chat_chat_id : data.chat,
+            User_user_id : data.user
+        })
+    })
+});
 
 server.get('/', async (req, res) => {
     await res.send("Root of the Willchair server.");
