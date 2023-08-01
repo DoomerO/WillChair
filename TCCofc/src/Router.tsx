b8720bf8cd20249a9e21462c252183eca8ff62a3
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-08-01 19:18:13 -0300

    Update router para login

--- TCCofc/src/Router.tsx
+++ TCCofc/src/Router.tsx
@@ -1,11 +1,10 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import Search from "./pages/Search";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import login from "./pages/Loginwip"
+import Login from "./pages/Login"
 import CreateOffer from "./pages/CreateOffer";
 import wcOffer from "./pages/wcOffer";
 import PageToggle from "./components/toggles/PageToggle";
@@ -28,8 +27,7 @@ const Router = () => {
         <BrowserRouter>
         <Routes>
             <Route Component={toggleHome} path="/"/>
-            <Route Component={login} path="/loginw"/>
-            <Route Component={Login} path="/login"/>
+            <Route Component={Login} path="/loginw"/>
             <Route Component={Search} path="/search/:query/:value"/>
             <Route Component={About} path="/about"/>
             <Route Component={Contact} path="/contact"/>
