2f11e09adc0feb8e635b4b29a963c947d9753efc
MatheusBarbosa-01 <101056776+MatheusBarbosa-01@users.noreply.github.com>
2023-08-03 08:42:58 -0300

    Update Profile.tsx

--- TCCofc/src/pages/Profile.tsx
+++ TCCofc/src/pages/Profile.tsx
@@ -1,9 +1,12 @@
 import Footer from "../components/Footer";
 import { useState,useEffect } from "react"
-import { Box} from '@chakra-ui/react';
+import { Box, Flex} from '@chakra-ui/react';
 import HeaderToggle from '../components/toggles/HeaderToggle';
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { Card, CardBody, Text, CardHeader, Heading, CardFooter, Button } from '@chakra-ui/react'
+import { Avatar, Wrap, WrapItem } from '@chakra-ui/react'
+import "../fonts/fonts.css"
 
 const Profile = () => {
   const {email} = useParams();
@@ -24,7 +27,26 @@ const Profile = () => {
   return (
     <Box w="100%" h="100%">
       <HeaderToggle/>
-     
+      <Flex h="120vh">
+      
+        <Card align='center' bgColor="#DFDFDF" h="70vh" w="60vw" margin="15vh 0 0 20vw" fontFamily="atkinson">
+          <CardHeader>
+            <Heading size='md'>Perfil do Usuário</Heading>
+          </CardHeader>
+          <CardBody>
+            <Wrap>
+              <WrapItem margin="0 0 0 3vw">
+                <Avatar name='Ryan Florence' src='https://bit.ly/ryan-florence' w="5vw" h="10vh"/>
+              </WrapItem>  
+            </Wrap>
+            <Text>Julio da Silva Santos</Text>
+          </CardBody>
+          <CardFooter>
+            <Button colorScheme='blue'>View here</Button>
+          </CardFooter>
+        </Card>
+
+      </Flex>
       <Footer/>
     </Box>
   )
