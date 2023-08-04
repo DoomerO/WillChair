81c252f832645f91e26f7777a9fee821170d75b3
BurstRange <113287175+BrRange@users.noreply.github.com>
2023-08-04 10:48:16 -0300

    Estilo

--- TCCofc/src/pages/Login.tsx
+++ TCCofc/src/pages/Login.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect, ChangeEvent } from 'react';
 import Password from '../components/Password';
 import axios from 'axios';
 import { useNavigate, Link, useParams } from 'react-router-dom';
-import { Wrap, Button, Collapse, Input, useBoolean, Container, useToast, Flex, useColorMode, ButtonGroup, Text } from '@chakra-ui/react'
+import { Wrap, Button, IconButton, Collapse, Input, useBoolean, Container, useToast, Flex, useColorMode, ButtonGroup, Text } from '@chakra-ui/react'
 import { FiArrowLeft, FiSun, FiMoon } from 'react-icons/fi'
 
 const Loginwip = () => {
@@ -98,32 +98,33 @@ const Loginwip = () => {
    const {toggleColorMode} = useColorMode();
     
     return (
-        <Container borderRadius='10px' border="solid #000 0.4vh" _dark={{border:"solid #1976D2 0.4vh"}} marginTop='10vh'
+        <Container borderRadius='10px' h="80vh" w="100vw" border="solid #DDD 0.4vh" _dark={{border:"solid #1976D2 0.4vh"}} marginTop='10vh'
             padding='2vh'  minH="80vh" minW="80%" maxW="fit-content">
-                <ButtonGroup spacing="80%" w="100%" justifyContent="center">
-                <Button><Link to="../"><FiArrowLeft/></Link></Button>
-                <Button onClick={toggleColorMode}>{
-                    localStorage.getItem("chakra-ui-color-mode") == 'light' ? <FiSun/> : <FiMoon/>
-                }</Button>
+                <ButtonGroup spacing="80%" w="100%" justifyContent="center" variant="ghost">
+                <Link to="../"><IconButton aria-label='Return to home' icon={<FiArrowLeft/>}></IconButton></Link>
+                <IconButton onClick={toggleColorMode} aria-label='switch lighting mode'
+                icon={localStorage.getItem("chakra-ui-color-mode") == 'light' ? <FiSun/> : <FiMoon/>}></IconButton>
                 </ButtonGroup>
                 <Collapse in={!screen}>
+                    <Text fontFamily="outfit" fontSize="200%" textAlign="center" w="50%" pb="3%" pt="3%">Login</Text>
                     <Wrap direction='row' spacing={0}>
-                        <Flex alignContent="center" w="50%" h="100%" direction="column">
+                        <Flex alignContent="center" w="46%" h="100%" direction="column" m="2%">
                         <Input className='submit' placeholder='E-mail' onChange={handleChange} name="email" value={fields.email} fontFamily="outfit"/>
                         <Password setTo={setPassword} value={password} placeholder='Senha'/>
                         <Button type='submit' onClick={handleLogin} fontFamily="outfit">Enviar</Button>
                         </Flex>
-                        <Flex alignContent="center" w="50%" h="100%" direction="column">
+                        <Flex alignContent="center" w="46%" h="100%" direction="column" m="2%">
                         <Button onClick={setScreen.toggle} fontFamily="outfit">Cadastro</Button>
                         </Flex>
                     </Wrap>
                 </Collapse>
                 <Collapse in={screen}>
+                <Text fontFamily="outfit" fontSize="200%" textAlign="center" w="50%" ml="50%" pb="3%" pt="3%">Cadastro</Text>
                     <Wrap direction='row' spacing={0}>
-                        <Flex alignContent="center" w="50%" h="100%" direction="column">
+                        <Flex alignContent="center" w="46%" h="100%" m="2%" direction="column">
                         <Button onClick={setScreen.toggle} fontFamily="outfit">Login</Button>
                         </Flex>
-                        <Flex alignContent="center" w="50%" h="100%" direction="column">
+                        <Flex alignContent="center" w="46%" h="100%" m="2%" direction="column">
                         <Input className='submit' placeholder='Nome de usuário' onChange={handleChange} name="name" value={fields.name} fontFamily="outfit"/>
                         <Input className='submit' placeholder='E-mail' onChange={handleChange} name="email" value={fields.email} fontFamily="outfit"/>
                         <Password setTo={setSubPass} value={subPass} placeholder='Senha'/>
