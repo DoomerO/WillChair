50d159ae6fc725570152b4e4281815c0650ff6bb
BurstRange <113287175+BrRange@users.noreply.github.com>
2023-08-04 22:38:54 -0300

    Opção de logout

--- TCCofc/src/pages/intersections/TokenErrorPage.tsx
+++ TCCofc/src/pages/intersections/TokenErrorPage.tsx
@@ -7,14 +7,18 @@ const TokenErrorPage = () => {
     return (
        <Box w="100%" h="100%" justifyContent="center">
             <Flex align="center" border="3px" direction="column" h="inherit" mt="10%">
-                <Heading as='h1' >Ops...Aconteceu um Erro!</Heading>
+                <Heading as='h1' >Sua conexão expirou!</Heading>
                 <Spacer/>
                 <Flex direction="column" align="center" mt="3%" mb="0.5%">
-                    <Text fontSize={{base: "23px", sm:"25px"}}>Parece que seu acessou acabou!</Text>
-                    <Text fontSize={{base: "23px", sm:"25px"}}>Clique no botão abaixo para renová-lo</Text>    
+                    <Text fontSize={{base: "23px", sm:"25px"}}>Clique no botão abaixo para renová-la</Text>    
                 </Flex>
                 <AiOutlineArrowDown size="5vh"/>
                 <Link to="/login"><Button variant="solid" colorScheme="linkedin" mt="0.5%">Login</Button></Link>
+                <Flex direction="column" align="center" mt="3%" mb="0.5%">
+                    <Text fontSize={{base: "23px", sm:"25px"}}>Ou desconecte</Text>   
+                </Flex>
+                <AiOutlineArrowDown size="5vh"/>
+                <Link to="/logout"><Button variant="solid" colorScheme="linkedin" mt="0.5%">Logout</Button></Link>
             </Flex>
        </Box>
     )
