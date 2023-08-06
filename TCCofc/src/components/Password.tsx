6c4a21f7bc3d0e3c4dad8a0d9c71396cb92399b1
BurstRange <113287175+BrRange@users.noreply.github.com>
2023-08-06 20:23:01 -0300

    Integração de validação de padrão para página de login

--- TCCofc/src/components/Password.tsx
+++ TCCofc/src/components/Password.tsx
@@ -1,26 +1,28 @@
 import { Input, useBoolean, InputRightAddon, InputGroup } from '@chakra-ui/react'
 import "../fonts/fonts.css"
-import { ChangeEvent } from 'react'
+import { ChangeEventHandler } from 'react'
+import { FiPlus } from 'react-icons/fi'
 
 interface passProps{
     placeholder?: string,
-    setTo: Function,
+    onChange: ChangeEventHandler<HTMLInputElement>,
     value?: string,
     pattern?: string
 }
-const Password = ({placeholder, setTo, value, pattern}:passProps) => {
+const Password = ({placeholder, onChange, value, pattern}:passProps) => {
     //Estado do botão de mostrar senha e valor da senha
     const [showPassword, setShowPassword] = useBoolean(false)
 
     return(
         <InputGroup>
         <Input placeholder={placeholder} type={showPassword ? 'text' : 'password'}
-        onChange={(e:ChangeEvent<HTMLInputElement>) => setTo(e.target.value)} value={value}
-        pattern={pattern ? pattern : "(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"} fontFamily="outfit"/>
+        onChange={onChange} value={value}
+        pattern={pattern ? pattern : `(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}`} fontFamily="outfit"/>
         
         <InputRightAddon bg={showPassword ? '#000' : '#fff'} color={showPassword ? '#fff' : '#000'}
         _dark={showPassword ? {bg:"#F7F9FC", color:"#484A4D"} : {bg:"#2D3748", color:"#1976D2"}}
-        onClick={setShowPassword.toggle} onMouseOut={setShowPassword.off} cursor='pointer'>+</InputRightAddon>
+        onClick={setShowPassword.toggle} onMouseOut={setShowPassword.off} cursor='pointer'>
+        <FiPlus/></InputRightAddon>
         </InputGroup>
     )
 }
