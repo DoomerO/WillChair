b8a36bb29fb4d4541473dfe36e78f714e24ff0cb
Gustav01v <123855261+Gustav01v@users.noreply.github.com>
2023-08-07 10:29:15 -0300

    Add files via upload

---
+++ TCCofc/src/pages/CurrentChats.tsx
@@ -0,0 +1,20 @@
+import HeaderToggle from "../components/toggles/HeaderToggle";
+import Footer from "../components/Footer";
+import {Box, Flex, Text, Grid, Center} from "@chakra-ui/react"
+
+
+const CurrentChats = () => {
+    return (
+        <Box w="100%" h="100%">
+            <HeaderToggle/>
+            <Center h="20vh"   pt="1%" bg="black" >aaaaaaaaaaaaa</Center>
+            <Grid  w="50vh" h="70vh"  pt="10%">
+                
+            </Grid>
+            <Footer/>
+        </Box>
+    )
+}
+
+export default CurrentChats;
+
