944f1dd87c4a0e799180ec51e3175a29f40bf196
Gustav01v <123855261+Gustav01v@users.noreply.github.com>
2023-08-08 10:51:23 -0300

    toggle de chats acessados

--- TCCofc/src/Router.tsx
+++ TCCofc/src/Router.tsx
@@ -8,7 +8,6 @@ import Login from "./pages/Login"
 import CreateOffer from "./pages/CreateOffer";
 import wcOffer from "./pages/wcOffer";
 import crOffer from "./pages/crOffer";
-import waOffer from "./pages/waOffer";
 import PageToggle from "./components/toggles/PageToggle";
 import HomeProd from "./pages/HomeProd";
 import ConfirmLogOut from "./pages/intersections/ConfirmLogOut";
@@ -16,6 +15,7 @@ import OfferPageLogged from "./pages/offerPages/OfferPageLogged";
 import OfferPage from "./pages/offerPages/OfferPage";
 import Report from "./pages/Report";
 import ProfileToggle from "./pages/profilePages/ProfileToggle";
+import CurrentChats from "./pages/CurrentChats";
 
 const Router = () => {
     const toggleHome = () => {
@@ -26,6 +26,9 @@ const Router = () => {
     }
     const toggleProfile = () => {
         return <ProfileToggle/>
+    }
+    const toggleCurchats = () => {
+        return <PageToggle compError={<Login />} compSuccess={<CurrentChats/>}/>
     }
 
     return (
@@ -41,10 +44,10 @@ const Router = () => {
             <Route Component={CreateOffer} path="/createoffer"/>
             <Route Component={wcOffer} path="/wcoffer"/>
             <Route Component={crOffer} path="/croffer"/>
-            <Route Component={waOffer} path="/waoffer"/>
             <Route Component={ConfirmLogOut} path="/logout"/>
             <Route Component={Report} path="/report/:offer"/>
             <Route Component={toggleProfile} path="/profile/:email/view"/>
+            <Route Component={toggleCurchats} path="/currentchats"/>
         </Routes>
     </BrowserRouter>
     )
