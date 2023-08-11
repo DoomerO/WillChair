cd920fed601315164564bbd2dc0753fc278e0f0f
Gustav01v <123855261+Gustav01v@users.noreply.github.com>
2023-08-11 10:46:30 -0300

    inicio de ideia

--- TCCofc/src/pages/CurrentChats.tsx
+++ TCCofc/src/pages/CurrentChats.tsx
@@ -1,16 +1,19 @@
 import HeaderToggle from "../components/toggles/HeaderToggle";
 import Footer from "../components/Footer";
-import {Box, Flex, Text, Grid, Center} from "@chakra-ui/react"
+import {Box, Flex, Text, Grid, Center, GridItem, Stack, Divider, Heading} from "@chakra-ui/react"
 import axios from "axios";
 import { useEffect, useState } from "react";
 import decode from "../components/decoderToken";
+import colors from "../colors/colors";
+import '../fonts/fonts.css';
+import SignNotFound from "../components/SignNotFound";
 
 const CurrentChats = () => {
 
     const [userToken, setToken] = useState(decode(localStorage.getItem("token")));
     const [user, setUser] = useState([]);
     const [chatsOthers, setChatsOthers] = useState([]);
-    const [offersIntrest, setIntrest] = useState([]);
+    const [offersInterest, setInterest] = useState([]);
 
     async function getUser() {
         await axios.get(`http://localhost:3344/user/email/${userToken.email}`, {
@@ -33,8 +36,8 @@ const CurrentChats = () => {
     }
 
     async function getOffersOthers(id : number) {
-        await axios.get(`http://localhost:3344/id/${id}`).then((res) => {
-            setIntrest(prev => ([...prev, res.data]))
+        await axios.get(`http://localhost:3344/offer/id/${id}`).then((res) => {
+            setInterest(prev => ([...prev, res.data]))
         }).catch((error) => {
             console.log(error)
         });
@@ -58,13 +61,25 @@ const CurrentChats = () => {
         }
     }, [chatsOthers])
 
+    const renderOfferOthers = offersInterest.map(item => {
+        return <div key={item.ofr_id}>
+            {item.ofr_name}
+        </div>
+    }) 
+
     return (
         <Box w="100%" h="100%">
             <HeaderToggle/>
-            <Center h="20vh"   pt="1%" bg="black" >aaaaaaaaaaaaa</Center>
-            <Grid  w="50vh" h="70vh"  pt="10%">
-                
-            </Grid>
+                <Flex direction="row" w="100%" h="100vh">
+                    <Stack w="25%" bg={colors.bgWhite} pt="5%" _dark={{bg : colors.bgWhite_Dark}} h="100%" borderRight="1px solid #000" align="center" >
+                        <Heading as="h2" fontSize={{base: "32px", sm: "29px"}} fontFamily="outfit" borderBottom="1px solid #000" w="100%" textAlign="center">Suas conversas</Heading>
+                        {(offersInterest.length > 0) ? renderOfferOthers : <SignNotFound msg="Você é anti-social, vai tomar no cu" icon={<div></div>}/>}
+                    </Stack>
+                    
+                    <Flex w="75%" bg={colors.bgWhite} _dark={{bg : colors.bgWhite_Dark}} h="100%">
+
+                    d</Flex>
+                </Flex>
             <Footer/>
         </Box>
     )
