74b25efa62f6b57642a171bc5b80da1bceb2b905
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-08-14 16:16:03 -0300

    Update Router

--- TCCofc/src/Router.tsx
+++ TCCofc/src/Router.tsx
@@ -5,10 +5,6 @@ import Search from "./pages/Search";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login"
-import CreateOffer from "./pages/CreateOffer";
-import wcOffer from "./pages/wcOffer";
-import waOffer from "./pages/waOffer";
-import crOffer from "./pages/crOffer";
 import PageToggle from "./components/toggles/PageToggle";
 import HomeProd from "./pages/HomeProd";
 import ConfirmLogOut from "./pages/intersections/ConfirmLogOut";
@@ -17,6 +13,7 @@ import OfferPage from "./pages/offerPages/OfferPage";
 import Report from "./pages/Report";
 import ProfileToggle from "./pages/profilePages/ProfileToggle";
 import CurrentChats from "./pages/CurrentChats";
+import OfferCreationToggle from "./pages/createOfferPages/OfferCreationToggle";
 
 const Router = () => {
     const toggleHome = () => {
@@ -27,6 +24,9 @@ const Router = () => {
     }
     const toggleProfile = () => {
         return <ProfileToggle/>
+    }
+    const toggleCretionOffer = () => {
+        return <PageToggle compSuccess={<OfferCreationToggle/>} compError={<div>teste</div>}/>
     }
     const toggleCurchats = () => {
         return <PageToggle compError={<Login />} compSuccess={<CurrentChats/>}/>
@@ -42,14 +42,11 @@ const Router = () => {
             <Route Component={About} path="/about"/>
             <Route Component={Contact} path="/contact"/>
             <Route Component={toggleOffer} path="/offer/:id"/>
-            <Route Component={CreateOffer} path="/createoffer"/>
-            <Route Component={wcOffer} path="/wcoffer"/>
-            <Route Component={waOffer} path="/waoffer"/>
-            <Route Component={crOffer} path="/croffer"/>
+            <Route Component={toggleCretionOffer} path="/create-offer/:type"/>
             <Route Component={ConfirmLogOut} path="/logout"/>
             <Route Component={Report} path="/report/:offer"/>
             <Route Component={toggleProfile} path="/profile/:email/view"/>
-            <Route Component={toggleCurchats} path="/currentchats"/>
+            <Route Component={toggleCurchats} path="/current-chats"/>
         </Routes>
     </BrowserRouter>
     )
