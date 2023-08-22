09cced428c1e00e5445e42687a1bdfdfc687e6c1
Gustav01v <123855261+Gustav01v@users.noreply.github.com>
2023-08-22 10:42:04 -0300

    correção de erro bobo

--- TCCofc/src/pages/Contact.tsx
+++ TCCofc/src/pages/Contact.tsx
@@ -12,11 +12,11 @@ const Contact = () => {
         <Box w="100%" h="100%">
             <HeaderToggle/>
             <Flex color="white" w="100%" h="100%" pt="5%" direction="row">
-                <Box bg="gray.100" p="6" borderRadius="md" shadow="md" w="40%">
+                <Box  p="6" borderRadius="md" shadow="md" w="40%">
                       <Heading as="h2" size="lg" mb="6" color="black">
                         Entre em Contato Conosco
                       </Heading>
-                <Flex direction="column" align="center" bg="green.500" p="4" borderRadius="md" color="white" mb="8">
+                <Flex direction="column" align="center" bg="green" p="4" borderRadius="md" color="white" mb="8">
                     <Link href="https://api.whatsapp.com/send?phone=SEU_NUMERO_DE_TELEFONE_AQUI" target="_blank" rel="noopener noreferrer">
                           <IoLogoWhatsapp size="36"/>
                           <Text mt="2" color="black">Entre em contato conosco pelo WhatsApp</Text>
@@ -32,7 +32,7 @@ const Contact = () => {
                     </Link>
                     <Link href="https://www.facebook.com/seu_perfil/" target="_blank" rel="noopener noreferrer" mt="4">
                         <BsFacebook size="36" color="blue" />
-                        <Text color="black" mt="2"></Text>Curta-nos no Facebook</Text>
+                        <Text color="black" mt="2">Curta-nos no Facebook</Text>
                     </Link>
       </Flex>
     </Box>
