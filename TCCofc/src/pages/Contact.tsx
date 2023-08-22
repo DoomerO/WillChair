db337ec9db708e25f2256f9c9e58a9fa6bde076a
Gustav01v <123855261+Gustav01v@users.noreply.github.com>
2023-08-21 23:31:19 -0300

    Add files via upload

--- TCCofc/src/pages/Contact.tsx
+++ TCCofc/src/pages/Contact.tsx
@@ -1,43 +1,52 @@
 import Footer from "../components/Footer";
-import { Box, Flex, Heading, Spacer, Stack } from '@chakra-ui/react';
+import { Box, Flex, Heading, Stack, Spacer, Center, Square, Text, AspectRatio, VStack, Container} from '@chakra-ui/react';
 import { IoLogoWhatsapp } from "react-icons/io"
 import { BsFacebook } from "react-icons/bs"
 import { ImInstagram } from "react-icons/im"
 import HeaderToggle from '../components/toggles/HeaderToggle';
-import { Input } from '@chakra-ui/react'
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+
 
 const Contact = () => {
     return (
-        <Box w="100%" h="100%"> 
-                    <HeaderToggle/>
-                    <Flex w='100%' align="center" pt='20vh' direction="column">
-                        <Heading color='#1976D2' fontSize="35px" textAlign="center" mb="10vh">Confira abaixo alguns dos nossas redes</Heading>
-                        
-                            <Flex>      
-                                <Stack spacing={3} >
-                                    <Input variant='filled' placeholder='Filled' />
-                                    <Input variant='filled' placeholder='Filled' />
-                                    <Input variant='filled' placeholder='Filled' />
-                                    <Input variant='filled' placeholder='Filled' />
-                                </Stack>  
-                            </Flex>     
-                            <Flex direction="row" w="50%">
-                                    <Link to='https://instagram.com/grupo_willchair?igshid=MzRIODBiNWFIZA=='><ImInstagram/> Instagram</Link>
-
-                                    <Spacer />
-                               
-                                    <Link to='https://wa.me/5524981267889?text=Descubra+a+liberdade+da+mobilidade+com+nossas+excepcionais+cadeiras+de+rodas.+Entre+em+contato+pelo+WhatsApp+na+Willchair.'><IoLogoWhatsapp/> Whatsapp</Link>
-                                
-                                    <Spacer/>
-                                
-                                    <Link to='https://www.facebook.com/people/Tcc-Willchair/pfbid02A9rF2B2yHXVPxS4oM841TwPHz3ain5wL9Neog5iHWDsRaNBVLsbEQZkFczi5WPm8l/?mibextid=%2FZbWKwL'> <BsFacebook/> Facebook </Link>
-                            </Flex>
-                       
-                    </Flex>
-                <Footer/>
-            </Box> 
-         
+        <Box w="100%" h="100%">
+            <HeaderToggle/>
+            <Flex color='white' w="100%" h="100%" pt="5%" direction="row">
+            <Box bg='gray.100' p={6} borderRadius="md" shadow="md" w="40%">
+      <Heading as="h2" size="lg" mb={6} color={"black"}>
+        Entre em Contato Conosco
+      </Heading>
+      <Flex direction="column" align="center" bg="green.500" p={4} borderRadius="md" color="white" mb={8}>
+        <Link href="https://api.whatsapp.com/send?phone=SEU_NUMERO_DE_TELEFONE_AQUI" target="_blank" rel="noopener noreferrer">
+          <IoLogoWhatsapp size={36} />
+          <Text mt={2} color="black">Entre em contato conosco pelo WhatsApp</Text>
+        </Link>
+      </Flex>
+      
+      <Heading as="h2" size="lg" mb={6} color={"black"} >
+        Nossas Redes Sociais
+      </Heading>
+      <Flex direction="column" align="center">
+        <Link href="https://www.instagram.com/seu_perfil/" target="_blank" rel="noopener noreferrer">
+          <ImInstagram size={36} color="purple" />
+          <Text color={"black"} mt={2} >Siga-nos no Instagram</Text>
+        </Link>
+        <Link href="https://www.facebook.com/seu_perfil/" target="_blank" rel="noopener noreferrer" mt={4}>
+          <BsFacebook size={36} color="blue" />
+          <Text color={"black"} mt={2}>Curta-nos no Facebook</Text>
+        </Link>
+      </Flex>
+    </Box>
+     
+                
+                <Stack w="60%" h="100%"  textAlign="center" direction="column" >
+                    <Heading color="blue">estamos aqui</Heading>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12345.6789!2d-123.456789!3d12.345678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTPCsDMxJzA1LjMiTiAxMjPCsDEwJzA2LjQiVw!5e0!3m2!1sen!2sus!4v1234567890123!5m2!1sen!2sus" 
+                        width="100%" height="600"/>
+                </Stack>
+            </Flex>
+            <Footer/>
+        </Box>
     )
 }
 
