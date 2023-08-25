3200b1d8365ab1c5ddf80cbb451455c37c4cf688
matador de porco 9000 <137410946+ointerrogacaodv@users.noreply.github.com>
2023-08-25 08:59:16 -0300

    Update avaliaçao.tsx
    "Assim como a águia voa com coragem pelos céus abertos, permita que sua determinação e confiança o conduzam através de qualquer desafio que a vida coloque em seu caminho, caro Barbosa."

--- TCCofc/src/pages/avaliaçao.tsx
+++ TCCofc/src/pages/avaliaçao.tsx
@@ -1,8 +1,6 @@
 import Footer from "../components/Footer";
 import { Box, Flex, Heading, Stack, Text,} from '@chakra-ui/react';
-import { IoLogoWhatsapp } from "react-icons/io"
-import { BsFacebook } from "react-icons/bs"
-import { ImInstagram } from "react-icons/im"
+import { IoLogoWhatsapp } from "react-icons/io"   
 import HeaderToggle from '../components/toggles/HeaderToggle';
 import { Link } from "react-router-dom";
 import "../fonts/fonts.css"
@@ -13,25 +11,47 @@ import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 interface RatingProps {
   maxStars: number;
 }
+import React, { useState } from 'react';
+import { VStack, Text, IconButton, Button } from '@chakra-ui/react';
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+
+interface RatingProps {
+  maxStars: number;
+}
 
 const Rating: React.FC<RatingProps> = ({ maxStars }) => {
   const [rating, setRating] = useState(0);
 
+  const handleRatingClick = (selectedRating: number) => {
+    setRating(selectedRating);
+  };
+
+  const handleSubmitRating = () => {
+ //
+    console.log(`Enviando avaliação: ${rating}`);
+  };
+
   return (
-    <VStack align="center">
+    <VStack align="center" spacing="4">
       <Text fontSize="lg" fontWeight="bold">
-        Rate this:
+        Avalie este usuário
+      </Text>
+      <Text fontSize="md">
+        {rating > 0 ? `Você avaliou com ${rating} estrela(s)` : 'Selecione uma avaliação'}
       </Text>
       <div>
         {[...Array(maxStars)].map((_, index) => (
           <IconButton
             key={index}
-            onClick={() => setRating(index + 1)}
+            onClick={() => handleRatingClick(index + 1)}
             icon={index < rating ? <AiFillStar /> : <AiOutlineStar />}
             colorScheme={index < rating ? 'yellow' : 'gray'}
           />
         ))}
       </div>
+      <Button colorScheme="blue" onClick={handleSubmitRating} disabled={rating === 0}>
+        Enviar Avaliação
+      </Button>
     </VStack>
   );
 };
