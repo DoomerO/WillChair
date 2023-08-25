140219d2851f5d15135c6d584e34448b7f177925
Gustav01v <123855261+Gustav01v@users.noreply.github.com>
2023-08-25 10:43:33 -0300

    Add files via upload

---
+++ TCCofc/src/pages/Avaliation.tsx
@@ -0,0 +1,21 @@
+import { VStack, Text, Button } from "@chakra-ui/react";
+
+const Avaliation = () => {
+  return (
+    <div>
+      <VStack align="center" spacing="4">
+      <Text fontSize="lg" fontWeight="bold">
+        Avalie este usuário
+      </Text>
+      <Text fontSize="md">
+      </Text>
+        
+      <Button colorScheme="blue" >
+        Enviar Avaliação
+      </Button>
+    </VStack>
+    </div>
+  )
+}
+
+export default Avaliation;
