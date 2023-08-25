a5ffe9f90133e1fde24b6afde990dc6100736af8
Gustav01v <123855261+Gustav01v@users.noreply.github.com>
2023-08-25 08:47:45 -0300

    Add files via upload

--- TCCofc/src/pages/Contact.tsx
+++ TCCofc/src/pages/Contact.tsx
@@ -13,8 +13,8 @@ const Contact = () => {
   return (
     <Box w="100%" h="100%">
       <HeaderToggle/>
-      <Flex w="100%" h="100%" pt="5%" direction="row" textAlign="center">
-        <Box  p="6" shadow="md" w="40%" bg={colors.veryLightBlue} _dark={{bg : colors.veryLightBlue_Dark}}>
+      <Flex w="100%" h="100%" direction="row" textAlign="center">
+        <Box  p="6" shadow="md" pt="5%" w="40%" bg={colors.veryLightBlue} _dark={{bg : colors.veryLightBlue_Dark}}>
           <Heading as="h2" size="lg" mb="6" fontFamily="outfit">
             Entre em Contato Conosco
           </Heading>
@@ -42,10 +42,10 @@ const Contact = () => {
             </Link>
         </Flex>
       </Box>
-      <Stack w="60%" h="100%"  textAlign="center" direction="column" bg={colors.bgWhite} _dark={{bg : colors.bgWhite_Dark}}>
+      <Stack w="60%" h="100%" pt="5%" textAlign="center" direction="column" bg={colors.bgWhite} _dark={{bg : colors.bgWhite_Dark}}>
         <Heading color={colors.colorFontBlue} fontFamily="outfit">Estamos aqui!</Heading>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3686.4366699714915!2d-44.072924925448845!3d-22.48779402302967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9ebd1700000001%3A0xe7458888a0206e3b!2sFAETEC%20Volta%20Redonda!5e0!3m2!1spt-BR!2sbr!4v1692837801377!5m2!1spt-BR!2sbr" 
-          width="100%" height="550"/>
+          width="100%" height="487" color-schme="dark"/>
       </Stack>
     </Flex>
     <Footer/>
