6293eb2b33658147b5402ca0cd80f55ef368bfcc
BurstRange <113287175+BrRange@users.noreply.github.com>
2023-09-11 09:55:32 -0300

    Sem mais erro da base de dados não existir
    Drop database if exists `willchair`

--- TCC-Server/database/willchair_database.sql
+++ TCC-Server/database/willchair_database.sql
@@ -14,7 +14,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema willchair
 -- -----------------------------------------------------
-Drop database willchair;
+Drop database if exists `willchair`;
 CREATE SCHEMA IF NOT EXISTS `willchair` DEFAULT CHARACTER SET utf8 ;
 USE `willchair` ;
 
