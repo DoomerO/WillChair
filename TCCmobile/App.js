6a352365b667e2044f08fda32e16301d2818b382
Mylliee <102260970+Mylliee@users.noreply.github.com>
2023-09-19 08:09:34 -0300

    Atualizando a home do mobile

--- TCCmobile/App.js
+++ TCCmobile/App.js
@@ -1,25 +1,49 @@
 import React from "react";
-import {  NativeBaseProvider, Box, Menu, Pressable, Divider, HamburgerIcon} from "native-base";
+import {  NativeBaseProvider, Box, Menu, Pressable, HamburgerIcon, Flex, Heading, Text} from "native-base";
+
+
 
 function Example() {
-  return <NativeBaseProvider><Box w="90%" alignItems="center" h="50%" marginTop="20%"> 
-      <Menu closeOnSelect={false} w="190" onOpen={() => console.log("opened")} onClose={() => console.log("closed")} trigger={triggerProps => {
+  return <NativeBaseProvider>
+    <Flex direction="column" w="100%" h="60%" bg="#F7F9FC" >
+      <Box w="50%" ml="85%" mt="7%"> 
+        <Menu closeOnSelect={false} w="295" onOpen={() => console.log("opened")} onClose={() => console.log("closed")} trigger={triggerProps => {
       return <Pressable {...triggerProps}>
-              <HamburgerIcon />
+              <HamburgerIcon size="8"/>
             </Pressable>;
     }}>
-        <Menu.OptionGroup defaultValue="Arial" title="free" type="radio">
-          <Menu.ItemOption value="Arial">Arial</Menu.ItemOption>
-          <Menu.ItemOption value="Nunito Sans">Nunito Sans</Menu.ItemOption>
-          <Menu.ItemOption value="Roboto">Roboto</Menu.ItemOption>
-        </Menu.OptionGroup>
-        <Divider mt="3" w="100%" />
-        <Menu.OptionGroup title="paid" type="checkbox">
-          <Menu.ItemOption value="SF Pro">SF Pro</Menu.ItemOption>
-          <Menu.ItemOption value="Helvetica">Helvetica</Menu.ItemOption>
-        </Menu.OptionGroup>
+          <Menu.ItemOption value="Contato">Home</Menu.ItemOption>
+          <Menu.ItemOption value="Pesquisa">Pesquisar equipamentos</Menu.ItemOption>
+          <Menu.ItemOption value="Contato">Contato</Menu.ItemOption>
+          <Menu.ItemOption value="Sobre">Sobre nós</Menu.ItemOption>
+          <Menu.ItemOption value="Entrar">Entrar</Menu.ItemOption>
+          <Menu.ItemOption value="Cadastrar">Cadastrar</Menu.ItemOption>
       </Menu>
     </Box>
+    
+    <Flex align='center' direction='column' mt="23%">
+      <Heading size="xl" color='#2D3748'>O seu sonho acessível</Heading>
+      <Heading color='#1976D2' size="xl">perto de você!</Heading>
+      <Text fontSize="14.5" noOfLines={2} mt='20px' textAlign="center">
+      Compre, negocie ou anuncie equipamentos de acessibilidade</Text>
+    </Flex>
+    </Flex>
+    
+    <Flex direction="column" w="100%">
+    <Heading as='h1' noOfLines={2} color="#2D3748" textAlign="center" mt="7%">
+    O que você pode encontrar por aqui</Heading>
+   
+      
+    </Flex>
+
+    <Flex w='100%' h='fit-content' bg='#F7F9FC' align='center' direction='column' _dark={{bg:'#484A4D'}}>
+                <Box h='20%' alignContent='center'>
+                <Heading fontFamily="outfit" as='h2' noOfLines={{base: 2, md: 1}}>
+                    Confira abaixo algumas de nossas categorias
+                </Heading>
+                </Box>
+    </Flex>
+
     </NativeBaseProvider>;
 }
 
