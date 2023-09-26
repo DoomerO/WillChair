20d4ffc41d3a6c9d329b80bfe66e2d4b855b58eb
Mylliee <102260970+Mylliee@users.noreply.github.com>
2023-09-26 10:53:02 -0300

    Tirando o header

--- TCCmobile/Routes.js
+++ TCCmobile/Routes.js
@@ -8,7 +8,7 @@ const Router = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={Home}/>
+                <Stack.Screen name="Home" component={Home} options={{headerShown : false}}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
